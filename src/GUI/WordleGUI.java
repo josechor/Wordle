94e54473ccb2ea84771f3efd0ce438bb5b7cdc7f
alumno <alumno@DAW1-14>
2022-05-09 17:16:04 +0200

    Interfaz casi terminada

--- src/GUI/WordleGUI.java
+++ src/GUI/WordleGUI.java
@@ -29,10 +29,9 @@ public class WordleGUI extends javax.swing.JFrame {
         java.awt.GridBagConstraints gridBagConstraints;
 
         BotonMotorGrupo = new javax.swing.ButtonGroup();
-        jPanel1 = new javax.swing.JPanel();
         Main = new javax.swing.JPanel();
-        letras = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
+        palabrasPanel = new javax.swing.JPanel();
+        letrasPanel = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
@@ -53,9 +52,17 @@ public class WordleGUI extends javax.swing.JFrame {
         jLabel23 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        Botones = new javax.swing.JPanel();
+        botonesPanel = new javax.swing.JPanel();
+        jPanel5 = new javax.swing.JPanel();
         jLabel6 = new javax.swing.JLabel();
         jLabel12 = new javax.swing.JLabel();
+        jPanel4 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jTextArea1 = new javax.swing.JTextArea();
+        jPanel3 = new javax.swing.JPanel();
+        jLabel24 = new javax.swing.JLabel();
+        jPanel6 = new javax.swing.JPanel();
+        jLabel25 = new javax.swing.JLabel();
         jMenuBar1 = new javax.swing.JMenuBar();
         jMenu1 = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
@@ -63,207 +70,242 @@ public class WordleGUI extends javax.swing.JFrame {
         jRadioButtonMenuItem1 = new javax.swing.JRadioButtonMenuItem();
         jRadioButtonMenuItem2 = new javax.swing.JRadioButtonMenuItem();
 
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 100, Short.MAX_VALUE)
-        );
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        Main.setLayout(new java.awt.GridLayout(0, 1));
+        Main.setLayout(new java.awt.BorderLayout());
 
-        letras.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+        palabrasPanel.setLayout(new java.awt.FlowLayout(java.awt.FlowLayout.CENTER, 0, 0));
 
-        jPanel2.setMaximumSize(new java.awt.Dimension(250, 250));
-        jPanel2.setMinimumSize(new java.awt.Dimension(250, 250));
-        jPanel2.setPreferredSize(new java.awt.Dimension(250, 250));
-        jPanel2.setLayout(new java.awt.GridLayout(4, 4, 4, 4));
+        letrasPanel.setMaximumSize(new java.awt.Dimension(250, 250));
+        letrasPanel.setMinimumSize(new java.awt.Dimension(250, 250));
+        letrasPanel.setPreferredSize(new java.awt.Dimension(250, 250));
+        letrasPanel.setLayout(new java.awt.GridLayout(4, 4, 4, 4));
 
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel3.setText("A");
         jLabel3.setToolTipText("");
         jLabel3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jLabel3.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel3.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel3.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel3);
+        letrasPanel.add(jLabel3);
 
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel4.setText("A");
         jLabel4.setToolTipText("");
         jLabel4.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel4.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel4.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel4);
+        letrasPanel.add(jLabel4);
 
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel5.setText("A");
         jLabel5.setToolTipText("");
         jLabel5.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel5.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel5.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel5);
+        letrasPanel.add(jLabel5);
 
+        jLabel7.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel7.setText("A");
         jLabel7.setToolTipText("");
         jLabel7.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel7.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel7.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel7);
+        letrasPanel.add(jLabel7);
 
+        jLabel8.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel8.setText("A");
         jLabel8.setToolTipText("");
         jLabel8.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel8.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel8.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel8);
+        letrasPanel.add(jLabel8);
 
+        jLabel9.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel9.setText("A");
         jLabel9.setToolTipText("");
         jLabel9.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel9.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel9.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel9);
+        letrasPanel.add(jLabel9);
 
+        jLabel10.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel10.setText("A");
         jLabel10.setToolTipText("");
         jLabel10.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel10.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel10.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel10);
+        letrasPanel.add(jLabel10);
 
+        jLabel11.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel11.setText("A");
         jLabel11.setToolTipText("");
         jLabel11.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel11.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel11.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel11);
+        letrasPanel.add(jLabel11);
 
+        jLabel13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel13.setText("A");
         jLabel13.setToolTipText("");
         jLabel13.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel13.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel13.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel13);
+        letrasPanel.add(jLabel13);
 
+        jLabel14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel14.setText("A");
         jLabel14.setToolTipText("");
         jLabel14.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel14.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel14.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel14);
+        letrasPanel.add(jLabel14);
 
+        jLabel15.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel15.setText("A");
         jLabel15.setToolTipText("");
         jLabel15.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel15.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel15.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel15);
+        letrasPanel.add(jLabel15);
 
+        jLabel16.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel16.setText("A");
         jLabel16.setToolTipText("");
         jLabel16.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel16.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel16.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel16);
+        letrasPanel.add(jLabel16);
 
+        jLabel17.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel17.setText("A");
         jLabel17.setToolTipText("");
         jLabel17.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel17.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel17.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel17);
+        letrasPanel.add(jLabel17);
 
+        jLabel19.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel19.setText("A");
         jLabel19.setToolTipText("");
         jLabel19.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel19.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel19.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel19);
+        letrasPanel.add(jLabel19);
 
+        jLabel20.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel20.setText("A");
         jLabel20.setToolTipText("");
         jLabel20.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel20.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel20.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel20);
+        letrasPanel.add(jLabel20);
 
+        jLabel21.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel21.setText("A");
         jLabel21.setToolTipText("");
         jLabel21.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel21.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel21.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel21);
+        letrasPanel.add(jLabel21);
 
+        jLabel22.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel22.setText("A");
         jLabel22.setToolTipText("");
         jLabel22.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel22.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel22.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel22);
+        letrasPanel.add(jLabel22);
 
+        jLabel23.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel23.setText("A");
         jLabel23.setToolTipText("");
         jLabel23.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel23.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel23.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel23);
+        letrasPanel.add(jLabel23);
 
+        jLabel2.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel2.setText("A");
         jLabel2.setToolTipText("");
         jLabel2.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel2.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel2.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel2);
+        letrasPanel.add(jLabel2);
 
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("A");
         jLabel1.setToolTipText("");
         jLabel1.setMaximumSize(new java.awt.Dimension(40, 40));
         jLabel1.setMinimumSize(new java.awt.Dimension(40, 40));
         jLabel1.setPreferredSize(new java.awt.Dimension(40, 40));
-        jPanel2.add(jLabel1);
+        letrasPanel.add(jLabel1);
 
-        letras.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 0, -1, -1));
+        palabrasPanel.add(letrasPanel);
 
-        Main.add(letras);
+        Main.add(palabrasPanel, java.awt.BorderLayout.PAGE_START);
 
-        Botones.setPreferredSize(new java.awt.Dimension(500, 230));
+        botonesPanel.setPreferredSize(new java.awt.Dimension(500, 230));
+        botonesPanel.setLayout(new java.awt.GridLayout(2, 2));
+
+        jPanel5.setLayout(new java.awt.GridLayout(2, 0));
 
         jLabel6.setText("jLabel6");
-        jLabel6.setMaximumSize(new java.awt.Dimension(60, 20));
-        jLabel6.setMinimumSize(new java.awt.Dimension(60, 20));
-        jLabel6.setPreferredSize(new java.awt.Dimension(60, 20));
+        jPanel5.add(jLabel6);
 
         jLabel12.setText("jLabel12");
-        jLabel12.setMaximumSize(new java.awt.Dimension(60, 20));
-        jLabel12.setMinimumSize(new java.awt.Dimension(60, 20));
-        jLabel12.setPreferredSize(new java.awt.Dimension(60, 20));
+        jPanel5.add(jLabel12);
 
-        javax.swing.GroupLayout BotonesLayout = new javax.swing.GroupLayout(Botones);
-        Botones.setLayout(BotonesLayout);
-        BotonesLayout.setHorizontalGroup(
-            BotonesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(BotonesLayout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(BotonesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jLabel12, javax.swing.GroupLayout.DEFAULT_SIZE, 220, Short.MAX_VALUE)
-                    .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(210, Short.MAX_VALUE))
+        botonesPanel.add(jPanel5);
+
+        jButton1.setText("jButton1");
+
+        jTextArea1.setColumns(1);
+        jTextArea1.setRows(1);
+        jTextArea1.setTabSize(5);
+        jTextArea1.setText("holas");
+        jTextArea1.setMaximumSize(new java.awt.Dimension(10, 10));
+        jTextArea1.setMinimumSize(new java.awt.Dimension(10, 10));
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel4Layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(13, 13, 13)
+                .addComponent(jTextArea1, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18))
         );
-        BotonesLayout.setVerticalGroup(
-            BotonesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(BotonesLayout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel12, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGap(27, 27, 27)
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jTextArea1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jButton1))
+                .addContainerGap(84, Short.MAX_VALUE))
         );
 
-        Main.add(Botones);
+        botonesPanel.add(jPanel4);
+
+        jPanel3.setLayout(new java.awt.GridBagLayout());
+
+        jLabel24.setText("has ganado a la 2");
+        jPanel3.add(jLabel24, new java.awt.GridBagConstraints());
+
+        botonesPanel.add(jPanel3);
+
+        jPanel6.setLayout(new java.awt.GridLayout());
+
+        jLabel25.setText("jLabel25");
+        jPanel6.add(jLabel25);
+
+        botonesPanel.add(jPanel6);
+
+        Main.add(botonesPanel, java.awt.BorderLayout.CENTER);
 
         jMenu1.setText("Reiniciar");
 
@@ -291,7 +333,7 @@ public class WordleGUI extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(Main, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 442, Short.MAX_VALUE)
+            .addComponent(Main, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 480, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -338,8 +380,9 @@ public class WordleGUI extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup BotonMotorGrupo;
-    private javax.swing.JPanel Botones;
     private javax.swing.JPanel Main;
+    private javax.swing.JPanel botonesPanel;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
@@ -355,6 +398,8 @@ public class WordleGUI extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel21;
     private javax.swing.JLabel jLabel22;
     private javax.swing.JLabel jLabel23;
+    private javax.swing.JLabel jLabel24;
+    private javax.swing.JLabel jLabel25;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
@@ -366,10 +411,14 @@ public class WordleGUI extends javax.swing.JFrame {
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenuBar jMenuBar1;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JPanel jPanel1;
-    private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel jPanel5;
+    private javax.swing.JPanel jPanel6;
     private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem1;
     private javax.swing.JRadioButtonMenuItem jRadioButtonMenuItem2;
-    private javax.swing.JPanel letras;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JPanel letrasPanel;
+    private javax.swing.JPanel palabrasPanel;
     // End of variables declaration//GEN-END:variables
 }
